66f1dfcd7438091a629a2f5bb560ed108f4c63ec
admin <admin@admins-MacBook-Air.local>
2016-12-22 00:43:39 +0000

    update redis

--- environments/production/modules/guestbook/manifests/redis.pp
+++ environments/production/modules/guestbook/manifests/redis.pp
@@ -1,9 +1,16 @@
 class guestbook::redis {
+
+  file{'/var/data':
+    ensure => directory,
+  }
+
   swarm_mode::service{"redis":
     service_replicas  => hiera('redis.replicas'),
     service_network   => "network1",
     service_name      => "redis",
+    service_mount     => [{"type"=>"bind","source"=>"/var/data","destination"=>"/data"}],
     service_d_image   => "redis",
-    service_x_options =>"redis-server --appendonly yes"
+    service_x_options => "redis-server --appendonly yes",
+    require           => File['/var/data']
   }
 }
